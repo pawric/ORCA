12b63b5e474a9b183412c13d5970b2302e769abf
Radosław Waśkiewicz <rwaskiewicz@wolterskluwer.pl>
2016-10-29 13:35:23 +0200

    Dodanie inicjalizacji testowej klasy TTestowyWarsztat jednym ze stanowisk - wstep do testów klasy TPlanowanie. Zarówno unit jak i klasta TTestowyWarsztat zostały dodane przez pomyłke w jednym w poprzednich commitów.

--- Warsztaty/PlanowanieZasobow/tests/Common/uTestowyWarsztat.pas
+++ Warsztaty/PlanowanieZasobow/tests/Common/uTestowyWarsztat.pas
@@ -11,10 +11,29 @@ type
     FStanowiska: TStanowiskoList;
     function GetStanowiska: TStanowiskoList;
   public
+    constructor Create;
     property Stanowiska: TStanowiskoList read GetStanowiska;
   end;
 
 implementation
+
+uses
+  SysUtils, uStanowisko, uPlanowanieTypes;
+
+constructor TTestowyWarsztat.Create;
+begin
+  inherited;
+  FStanowiska.Add(
+    TStanowisko.Create(
+      [teSamochod],
+      EncodeDate(2016, 1,1) + EncodeTime(18,0,0,0),
+      EncodeDate(2016, 1,1) + EncodeTime(8,0,0,0),
+      'Myjnia bezdotykowa',
+      1,
+      [ruMycie]
+    )
+  );
+end;
 
 { TTestowyWarsztat }
 

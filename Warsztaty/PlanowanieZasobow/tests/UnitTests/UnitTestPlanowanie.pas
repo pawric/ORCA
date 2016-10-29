80c139bb3a69289653b49c4b1b0ac82dbe34f351
Radosław Waśkiewicz <rwaskiewicz@wolterskluwer.pl>
2016-10-29 14:24:24 +0200

    Dodanie nowych check'ów do testu metody TPlanowanie.WykonajRezerwacje. Po dodaniu nowych sprawdzeń doszło do śmiesznej i ciekawej sytuacji. Test wysiada ponieważ wartości rozpoczęcia prac nie są zgodne z oczekiwanymi (tak mówi DUnit) a kod mówi co innego ;) Może, ktos będzie miał jakiś pomysł ?

--- Warsztaty/PlanowanieZasobow/tests/UnitTests/UnitTestPlanowanie.pas
+++ Warsztaty/PlanowanieZasobow/tests/UnitTests/UnitTestPlanowanie.pas
@@ -49,6 +49,12 @@ begin
   );
 
   CheckNotNull(v_Rezerwacja, 'nie utworzono rezerwacji w pustym i dostepnym warsztacie');
+  CheckEquals('100000',v_Rezerwacja.NumerKlienta);
+  CheckEquals(EncodeDate(2016,1,1)+EncodeTime(8,0,0,0),v_Rezerwacja.RozpoczeciePrac, 'Data i czas rozpoczecia prac nie pasuja do oczekiwanego');
+  CheckEquals(EncodeDate(2016,1,1)+EncodeTime(8,0,0,0),v_Rezerwacja.ZakonczeniePrac, 'Data i czas zakonczenia prac nie pasuja do oczekiwanego');
+  CheckTrue(v_Rezerwacja.RodzajUslugi = ruMycie);
+  CheckTrue(v_Rezerwacja.Rodzaj = teSamochod);
+  CheckTrue(v_Rezerwacja.Status = seRezerwacja);
 end;
 
 end.
